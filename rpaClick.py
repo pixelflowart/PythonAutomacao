b22259b0280ae6997cc74be3a041f7ab9f71a329
pixelflowart <danielbruno84@gmail.com>
2024-01-11 04:50:32 -0300

    rpaCriaPasta

---
+++ rpaClick.py
@@ -0,0 +1,3 @@
+import pyautogui
+
+pyautogui.click(x=383,y=376, clicks=500,interval=0.1,button='left',duration=2)
---
+++ rpaCriaPasta.py
@@ -0,0 +1,9 @@
+import pyautogui
+
+pyautogui.moveTo(383,376, duration=1)
+pyautogui.click(button='right')
+pyautogui.moveTo(483,526, duration=1)
+pyautogui.click()
+pyautogui.moveTo(723,26, duration=1)
+pyautogui.click()
+pyautogui.click()
